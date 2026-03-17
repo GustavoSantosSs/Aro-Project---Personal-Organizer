c023f55a378d34669fd0e747c367ea969fb8ae97
Gustavo <grds1234@outlook.com>
2026-03-17 09:38:42 -0300

    Estilização de botões

--- src/Components/PainelNextTasks/Header/Filter/Filter.jsx
+++ src/Components/PainelNextTasks/Header/Filter/Filter.jsx
@@ -3,7 +3,7 @@ import SubFilter from "./Sub/SubFilter";
 
 function NextTasksFilter() {
     return (
-        <section id="NextTasksFilter-container"class="w-[65%] flex gap-5 mt-[10px]">
+        <section id="NextTasksFilter-container"class="w-[75%] flex gap-5 mt-[10px]">
         {/* Mudança com Click no botão de Filtro: border-2 border-white rounded-[10px] bg-[#9999992b] */}
             <section id="MainFilter-container" class="w-[70%] justify-end flex">
                 <MainFilter />
--- src/Components/PainelNextTasks/Header/Filter/Sub/SubFilter.jsx
+++ src/Components/PainelNextTasks/Header/Filter/Sub/SubFilter.jsx
@@ -1,7 +1,12 @@
 function SubFilter() {
     return (
-    <ul>
-        <li><h1 class="text-white">G1</h1></li>
+    <ul class="flex flex-wrap gap-2 flex-col items-start absolute">
+        <li class="flex bg-gradient-to-r from-[#f77] to-[#f93636] p-2 rounded-[10px] cursor-default">  
+            <h1 class="text-white bg-[#333] rounded-[10px] cursor-default text-[40px] font-bold px-2">G1</h1>
+        </li>
+        <li class="flex bg-gradient-to-r from-[#f77] to-[#f93636] p-2 rounded-[10px] cursor-default">  
+            <h1 class="text-white bg-[#333] rounded-[10px] cursor-default text-[40px] font-bold px-2">G2</h1>
+        </li>
     </ul>
     )
 }
